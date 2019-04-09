4e473b9d6e0d51f90319eaedb4d227045bf8de36
Josh Sample <joshuawsample@gmail.com>
2019-04-09 17:24:45 -0400

    Auth functionality started, ready to add Redux

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -30,11 +30,11 @@ class GoogleAuth extends Component {
     });
   }
 
-  onSignIn = () => {
+  onSignInClick = () => {
     this.auth.signIn();
   }
 
-  onSignOut = () => {
+  onSignOutClick = () => {
     this.auth.signOut();
   }
 
@@ -43,14 +43,14 @@ class GoogleAuth extends Component {
       return null;
     } else if (this.state.isSignedIn) {
       return (
-      <button onClick={this.onSignOut} className="ui red google button">
+      <button onClick={this.onSignOutClick} className="ui red google button">
         <i className="google icon" />
         Sign Out
       </button>
       )
     } else {
       return (
-      <button onClick={this.onSignIn} className="ui red google button">
+      <button onClick={this.onSignInClick} className="ui red google button">
         <i className="google icon" />
         Sign In with Google
       </button>
